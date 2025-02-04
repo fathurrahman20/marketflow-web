a8c81036b9643426f4ed7c9c7eacea017c3facce
Fathurrahman <10190031@bsi.ac.id>
2025-02-04 19:18:08 +0700

    fix: add product to cart, validation form sign-in & sign-up

--- src/components/home/card-categories.tsx
+++ src/components/home/card-categories.tsx
@@ -7,7 +7,7 @@ export default function CardCategories({ name }: Props) {
   return (
     <Link to={`/products?category=${name}`}>
       <div className="card flex-none w-[180px] h-[180px] rounded-sm flex flex-col items-center justify-center border-[#B3B3B3] border-[2px] py-4 hover:bg-[#DB4444] hover:text-white transition-colors ease-linear duration-200 cursor-pointer">
-        <p className="mt-4 px-3 text-center">{name}</p>
+        <p className="px-3 mt-4 text-center capitalize">{name}</p>
       </div>
     </Link>
   );
--- src/pages/cart-page.tsx
+++ src/pages/cart-page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import useTitlePage from "@/hooks/useTitlePage";
 import CartOrderSummary from "@/components/cart/cart-order-summary";
 import CartProduct from "@/components/cart/cart-product";
 import Layout from "@/components/layout";
 import { Skeleton } from "@/components/ui/skeleton";
 import useCart from "@/hooks/useCarts";
-import useTitlePage from "@/hooks/useTitlePage";
+import { ShoppingCart } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default function CartPage() {
   useTitlePage("Cart");
@@ -18,8 +21,8 @@ export default function CartPage() {
     <Layout>
       <div className="bg-white">
         <main>
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="mx-auto max-w-4xl pt-16">
+          <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="max-w-4xl pt-16 mx-auto">
               <h1 className="text-3xl font-bold tracking-tight text-gray-900">
                 Shopping Cart
               </h1>
@@ -32,16 +35,16 @@ export default function CartPage() {
 
                   <ul
                     role="list"
-                    className="divide-y divide-gray-200 border-b border-t border-gray-200">
+                    className="border-t border-b border-gray-200 divide-y divide-gray-200">
                     {isLoading && (
                       <div className="flex flex-row py-5">
                         <Skeleton className="h-[100px] w-[110px] my-auto" />
-                        <div className="w-full items-center justify-between px-5 flex flex-row">
-                          <div className="flex flex-col gap-y-16 justify-center">
+                        <div className="flex flex-row items-center justify-between w-full px-5">
+                          <div className="flex flex-col justify-center gap-y-16">
                             <Skeleton className="h-4 w-[100px]" />
                             <Skeleton className="h-4 w-[100px]" />
                           </div>
-                          <div className="flex flex-col gap-y-16 justify-center">
+                          <div className="flex flex-col justify-center gap-y-16">
                             <Skeleton className="h-4 w-[180px]" />
                             <Skeleton className="h-4 w-[180px]" />
                           </div>
@@ -51,11 +54,24 @@ export default function CartPage() {
                     {data?.data?.items.map((item) => (
                       <CartProduct item={item} key={item.id} />
                     ))}
+                    {!isLoading && data?.data?.items.length === 0 && (
+                      <div className="my-4">
+                        <Alert className="mx-auto md:text-center">
+                          <ShoppingCart className="w-4 h-4 md:hidden" />
+                          <AlertTitle>Your cart is currently empty</AlertTitle>
+                          <AlertDescription>
+                            Please add items before proceeding.
+                          </AlertDescription>
+                        </Alert>
+                      </div>
+                    )}
                   </ul>
                 </section>
 
                 {/* Order summary */}
-                <CartOrderSummary totalPrice={totalPrice ? totalPrice : 0} />
+                {!isLoading && data?.data?.items.length !== 0 && (
+                  <CartOrderSummary totalPrice={totalPrice ? totalPrice : 0} />
+                )}
               </form>
             </div>
           </div>
