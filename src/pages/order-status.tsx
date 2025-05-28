9c41b3f450aa51fb53e5cd009b0ca06759372e5e
Fathurrahman <10190031@bsi.ac.id>
2025-05-28 20:10:56 +0700

    fix: order status page

--- src/pages/order-status.tsx
+++ src/pages/order-status.tsx
@@ -1,9 +1,8 @@
-import { useSearchParams } from "react-router";
+// import { useSearchParams } from "react-router";
 
 export default function OrderStatusPage() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [searchParams, setSearchParams] = useSearchParams();
-  const transactionId = searchParams.get("transaction_id");
-  console.log(transactionId);
+  //   const [searchParams, setSearchParams] = useSearchParams();
+  //   const transactionId = searchParams.get("transaction_id");
+  //   console.log(transactionId);
   return <div>OrderStatusPage</div>;
 }
